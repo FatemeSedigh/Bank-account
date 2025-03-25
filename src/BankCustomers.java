963a808bad184262ad52a50b1a64c792c1233013
fati <fatemesadatsedigh@gmail.com>
2025-03-25 13:13:31 +0330

    writing-BankCustomers

--- src/BankCustomers.java
+++ src/BankCustomers.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
 public class BankCustomers {
+
+    private ArrayList<BankAccount> accountsList;
+
+    public void addAccount(BankAccount account) {
+        accountsList.add(account);
+    }
+
+    public void showAllBalances(){
+        System.out.println("\nلیست تمام حساب‌ها:");
+        for (BankAccount account : accountsList) {
+            System.out.println("شماره حساب: " + account.getAccountNumber() + ", نام: " + account.getAccountHolderName() + ", موجودی: " + account.getBalance());
+        }
+    }
+
+    public BankAccount findAccount(String accountNumber){
+        for (BankAccount account : accountsList){
+            if(account.getAccountNumber().equals(accountNumber)){
+                System.out.println("شماره حساب: " + account.getAccountNumber() + ", نام: " + account.getAccountHolderName() + ", موجودی: " + account.getBalance());
+                return account;
+            }
+        }
+        System.out.println("حساب با شماره " + accountNumber + " یافت نشد!");
+        return null;
+    }
 }
