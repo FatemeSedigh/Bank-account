049dc229f6aee7b3f6815223412c6e69923b595f
fati <fatemesadatsedigh@gmail.com>
2025-03-25 13:01:32 +0330

    writing-TransactionAccount

--- src/TransactionAccount.java
+++ src/TransactionAccount.java
@@ -1,2 +1,31 @@
 public class TransactionAccount extends BankAccount{
+
+    private final double overDraftLimit = 500 ;
+
+    public AccountTransaction (String accountHolderName, String accountNumber, double initialBalance){
+        super(accountHolderName,accountNumber,initialBalance);
+    }
+
+    @Override
+    public void calculateInterest(){
+        System.out.println("حساب جاری سود ندارد");
+    }
+
+    @Override
+    Public void withdraw(double amount){
+        if (amount < 0){
+            System.out.println("مبلغ برداشتی نامعتبر است!");
+        }
+        else if (amount > balance + overDraftLimit){
+            System.out.println("موجودی کافی نیست");
+        }
+        else {
+            balance -= amount;
+            System.out.println(amount + " تومان از حساب جاری شماره " + getAccountNumber() + " برداشت شد.");
+        }
+    }
+
 }
+
+//        if (amount <= balance + overDraftLimit){}
+
