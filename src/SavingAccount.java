02e2f985039778eef06219e4e9d234b62e317adc
fati <fatemesadatsedigh@gmail.com>
2025-03-25 12:47:49 +0330

    writing-SavingsAccount

--- src/SavingAccount.java
+++ src/SavingAccount.java
@@ -1,2 +1,15 @@
 public class SavingAccount extends BankAccount{
+
+    private final double interestRate = 0.03 ;
+
+    public SavingsAccount(String accountNumber, String accountHolderName, double initialBalance){
+        super(accountNumber, accountHolderName, initialBalance);
+    }
+
+    @Override
+    public void calculateInterest() {
+        double interest = balance * interestRate;
+        deposit(interest);
+        System.out.println("سود حساب پس‌انداز به مبلغ " + interest + " تومان محاسبه و واریز شد.");
+    }
 }
